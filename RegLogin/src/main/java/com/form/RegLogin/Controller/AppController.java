486f1d06181a5f02a8a42499d8847f78705f0834
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-04 19:29:36 +0200

    regLog, not working login

--- RegLogin/src/main/java/com/form/RegLogin/Controller/AppController.java
+++ RegLogin/src/main/java/com/form/RegLogin/Controller/AppController.java
@@ -3,10 +3,17 @@ package com.form.RegLogin.Controller;
 import com.form.RegLogin.Data.Entity.User;
 import com.form.RegLogin.Data.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @Controller
 public class AppController {
 
@@ -23,5 +30,25 @@ public class AppController {
         model.addAttribute("user", new User());
         return "signup_form";
     }
+
+    @PostMapping("/process_register")
+    public String processRegister(User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+        userRepo.save(user);
+        return "register_success";
+    }
+
+    @GetMapping("/users")
+    public String listUsers(Model model) {
+        List<User> listUsers = userRepo.findAll();
+        model.addAttribute("listUsers", listUsers);
+
+        return "users";
+    }
+
+
 
 }
