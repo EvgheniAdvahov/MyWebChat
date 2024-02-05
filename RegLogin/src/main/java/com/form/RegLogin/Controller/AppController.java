2b4eda68fa2c1eb76ff162324177adef60f00fbf
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-05 20:49:19 +0200

    Reg log  email auth, working fine credential))

--- RegLogin/src/main/java/com/form/RegLogin/Controller/AppController.java
+++ RegLogin/src/main/java/com/form/RegLogin/Controller/AppController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.repository.query.Param;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
