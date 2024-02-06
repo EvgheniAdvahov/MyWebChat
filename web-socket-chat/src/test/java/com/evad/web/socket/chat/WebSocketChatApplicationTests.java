74c3f81058530bbe16e8e15f52980b3348cc52f8
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-06 22:02:15 +0200

    web socket chat

---
+++ web-socket-chat/src/test/java/com/evad/web/socket/chat/WebSocketChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evad.web.socket.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
