29131ca3446bfea9c0c4b0e2bc7b88ff14f949fe
EvgheniAdvahov <evghenii.88@gmail.com>
2024-02-07 21:23:56 +0200

    docker container installed

---
+++ websocket-chat-one-to-one/src/main/java/com/evad/websocketchatonetoone/WebsocketChatOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.evad.websocketchatonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketChatOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketChatOneToOneApplication.class, args);
+	}
+
+}
---
+++ websocket-chat-one-to-one/src/test/java/com/evad/websocketchatonetoone/WebsocketChatOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evad.websocketchatonetoone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketChatOneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
